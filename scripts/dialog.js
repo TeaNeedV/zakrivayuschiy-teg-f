8b2735656785823090272f694b1cda30b0a33cae
TeaNeedV <teanidd@yandex.ru>
2025-07-01 21:10:37 +0300

    Add files via upload

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -2,10 +2,10 @@
     const openButton = document.querySelector('.button-safe');
     const dialog = document.getElementById('dialog-id');
     const closeButton = document.getElementById('closeButton');
-          closeButton.addEventListener('click', function() {dialog.close();
-            });
-    openButton.addEventListener('click', function() {
-      dialog.showModal();
-    });
-    
-  
+   closeButton.addEventListener('click', function() {
+  dialog.close();
+});
+
+openButton.addEventListener('click', function() {
+  dialog.showModal();
+});
